b197d5ecdfdc1561f0daa8f18babcaaaf3003840
EvLast <3178146280@qq.com>
2024-06-07 03:22:42 +0800

    完成Student登录页面

---
+++ StudentHome/StudentHome/StudentView.h
@@ -0,0 +1,41 @@
+#pragma once
+
+/*
+ Copyright WU DE QUAN. All rights reserved.
+
+ @Author: EvLast
+ @Date: 2024/6/7 0:20:30
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+#ifndef _STUDENTVIEW_
+#define _STUDENTVIEW_
+
+#include<iostream>
+#include <graphics.h>
+#include <conio.h>
+#include <string>
+#include <ctime>
+// -------------------- 数据设计 --------------------
+
+
+
+// -------------------- service --------------------
+
+// 学生页面登入窗口
+void LoginView();
+
+// -------------------- view --------------------
+
+#endif // !_STUDENT_
---
+++ StudentHome/StudentHome/admin.cpp
@@ -0,0 +1,6 @@
+#include "admin.h"
+
+
+
+
+bool resign(std::string name, std::string power);
